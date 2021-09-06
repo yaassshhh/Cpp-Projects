b5c808c24e576553fe501d2d7a09e4c07021adac
yaassshhh <85840807+yaassshhh@users.noreply.github.com>
2021-09-06 16:51:41 +0530

    Create 11_even_odd.cpp

---
+++ 11_even_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number to check it is even or add : ";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Enter number is even";
+    else 
+    cout<<"Enter number is odd";
+
+    return 0; 
+}
