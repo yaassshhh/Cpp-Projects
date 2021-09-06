1d915c731282d37a900604739926822cb8e58040
yaassshhh <85840807+yaassshhh@users.noreply.github.com>
2021-09-06 16:43:50 +0530

    Create 3rd_airthmatic_opreations.cpp

---
+++ 3rd_airthmatic_opreations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter two number two perform Arithmetic Operations :" << endl;
+    cin >> a >> b;
+    cout << "sum of " << a << " " << b << " is : " << endl;
+    cout << a + b << endl;
+    cout << "Sub of " << a << " " << b << " is : " << endl;
+    cout << a - b << endl;
+    cout << "Mul of " << a << " " << b << " is : " << endl;
+    cout << a * b << endl;
+    cout << "Div of " << a << " " << b << " is : " << endl;
+    cout << a / b << endl;
+    cout << "Remiander of " << a << " " << b << " is : " << endl;
+    cout << a % b << endl;
+    return 0;
+}
