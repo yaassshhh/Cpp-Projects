757c20a4bca4354fd126add391352266eb4f00c4
yaassshhh <85840807+yaassshhh@users.noreply.github.com>
2021-09-06 16:47:07 +0530

    Create 9th_comparenumber.cpp

---
+++ 9th_comparenumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+
+    int num1, num2;
+    cout<<"Enter two number to compare : ";
+    cin>>num1>>num2;
+    if(num1==num2)
+    {
+        cout<<"Given numbers are equal  ";
+    }
+    else 
+    cout<<"Given numbers not are equal  ";
+
+    return 0 ;
+}
