21ff6cb4fe4df5264aca7a292b36fe75e54cc22c
yaassshhh <85840807+yaassshhh@users.noreply.github.com>
2021-09-06 17:12:26 +0530

    Update 26_pyaramid.cpp

--- 26_pyaramid.cpp
+++ 26_pyaramid.cpp
@@ -1,7 +1,4 @@
 /* 
-NAME : RAHUL MUKATI
-ENO :  0827CI201150
-CSIT -3 
 program -26 “Write a function in C++ to generate the following Pyramid of Numbers and stars”
 
                                                      0
