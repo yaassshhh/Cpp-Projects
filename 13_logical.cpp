c952b5a1c6a645ed876e3cbb411e2afbea0eebf0
yaassshhh <85840807+yaassshhh@users.noreply.github.com>
2021-09-06 17:05:53 +0530

    Update 13_logical.cpp

--- 13_logical.cpp
+++ 13_logical.cpp
@@ -1,7 +1,5 @@
 /* 
-NAME : RAHUL MUKATI
-ENO :  0827CI201150
-CSIT -3 
+
 program -13 “Write a Program for Bitwise Logical Operators, Increment Operators and Ternary Operators”*/
 
 #include<iostream>
